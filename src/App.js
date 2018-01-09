c1cc995226fe607630fac37e136278b52f207bcf
fabien.brun <fabienbrun@hotmail.fr>
2018-01-09 19:37:08 +0100

    add params for firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+
 class App extends Component {
+componentWillMount() {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyCNF2TN-o_s1RiVs8JVm7SX3Y7vp0sws30',
+    authDomain: 'authentification-9b9ed.firebaseapp.com',
+    databaseURL: 'https://authentification-9b9ed.firebaseio.com',
+    projectId: 'authentification-9b9ed',
+    storageBucket: 'authentification-9b9ed.appspot.com',
+    messagingSenderId: '382145171081'
+  }
+);
+}
+
   render() {
     return (
       <View>
