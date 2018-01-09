848009d309035745046948b0c18e44e3e379e91b
fabien.brun <fabienbrun@hotmail.fr>
2018-01-09 08:51:53 +0100

    add common component

---
+++ src/components/common/Header.js
@@ -0,0 +1,34 @@
+// Import librairies
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// Make component
+const Header = (props) => {
+  const { viewStyle, textStyle } = styles;
+  return (
+      <View style={viewStyle}>
+          <Text style={textStyle}>{props.headerText}</Text>
+      </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+      
+      backgroundColor: '#F8F8F8',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 60,
+      paddingTop: 15,
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.2,
+      position: 'relative'
+  }, 
+  textStyle: {
+    justifyContent: 'center',
+    fontSize: 20
+  }
+};
+
+// Make the component available to other parts of the app
+export default Header;
