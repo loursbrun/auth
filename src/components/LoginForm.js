74be39fa01dee2801e72dca7c4cba95d8fa8652e
fabien.brun <fabienbrun@hotmail.fr>
2018-01-12 19:07:40 +0100

    change password

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common'
 
 class LoginForm extends Component {
-state = { email:'', password: '' };
+state = { email:'', password: '', error: '' };
+
+onButtonPress() {
+    const { email, password } = this.state;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch(() => {
+      
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch(() => {
+         thhs.setState({ error: 'Authentification Failed' }); 
+        });
+    });
+}
 
     render() {
         return(
@@ -26,13 +40,25 @@ state = { email:'', password: '' };
                     />
                 </CardSection>
 
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
+
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Log In 
                     </Button>
                 </CardSection>
             </Card>
         );
+    }
+}
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
     }
 }
 
